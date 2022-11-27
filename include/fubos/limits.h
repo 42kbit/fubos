568ab6c20102fd7f8a41ea0b8c80b35211cb8d93
Serhii Ostrikov <serostrikov@gmail.com>
2022-11-27 21:57:18 +0200

    limits.h

---
+++ include/fubos/limits.h
@@ -0,0 +1,13 @@
+#ifndef __H_FUBOS_LIMITS_H
+#define __H_FUBOS_LIMITS_H
+
+#include <fubos/assert.h>
+
+__ASSERT(sizeof(int) == 4)
+
+#define UINT_MAX 0xffffffff
+
+#define INT_MAX 0x7fffffff
+#define INT_MIN (-INT_MAX - 1)
+
+#endif /* __H_FUBOS_LIMITS_H */
