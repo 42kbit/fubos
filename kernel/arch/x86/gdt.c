6f64b6df35cab3d93e1ebe730f6803fbe7df69ec
Serhii Ostrikov <serostrikov@gmail.com>
2023-01-02 07:36:17 +0200

    bitfields cleaned, removed unneeded gdt load function

--- kernel/arch/x86/gdt.c
+++ kernel/arch/x86/gdt.c
@@ -32,15 +32,15 @@
 #define UDATA_FLAGS\
 	(GDT_WRITEABLE | GDT_CORD | GDT_PRIV(3) | GDT_PRESENT)
 
-struct gdt_node gdt[GDT_NENT] = {
-	[GDT_IDX_NULL] = {0},
-	[GDT_IDX_KCODE] = gdt_make_node(0, 0xfffff, KCODE_FLAGS, GDT_32_ACCESS),
-	[GDT_IDX_KDATA] = gdt_make_node(0, 0xfffff, KDATA_FLAGS, GDT_32_ACCESS),
-	[GDT_IDX_UCODE] = gdt_make_node(0, 0xfffff, UCODE_FLAGS, GDT_32_ACCESS),
-	[GDT_IDX_UDATA] = gdt_make_node(0, 0xfffff, UDATA_FLAGS, GDT_32_ACCESS)
+struct gdt_entry gdt[GDT_NENT] = {
+	[GDT_IDX_NULL]  = {0},
+	[GDT_IDX_KCODE] = gdt_make_entry(0, 0xfffff, KCODE_FLAGS, GDT_32_ACCESS),
+	[GDT_IDX_KDATA] = gdt_make_entry(0, 0xfffff, KDATA_FLAGS, GDT_32_ACCESS),
+	[GDT_IDX_UCODE] = gdt_make_entry(0, 0xfffff, UCODE_FLAGS, GDT_32_ACCESS),
+	[GDT_IDX_UDATA] = gdt_make_entry(0, 0xfffff, UDATA_FLAGS, GDT_32_ACCESS)
 };
 
 struct gdt_ptr gdt_ptr = {
-	.size = GDT_NENT * sizeof(struct gdt_node) - 1,
+	.size = GDT_NENT * sizeof(struct gdt_entry) - 1,
 	.ptr = (u32)&gdt
 };
--- kernel/arch/x86/include/asm/gdt.h
+++ kernel/arch/x86/include/asm/gdt.h
@@ -50,58 +50,34 @@
 #include <fubos/compiler_attributes.h>
 #include <fubos/ints.h>
 
-#define gdt_make_node(_base, _limit, _access, _flags)\
-	{\
-		.limit_low 	= bitcut(_limit, 0, 16),\
-		.base_low 	= bitcut(_base, 0, 16),\
-		.base_mid	= bitcut(_base, 16, 8),\
-		.a 		= bitcut(_access,0, 1),\
-		.rw 		= bitcut(_access,1, 1),\
-		.dc		= bitcut(_access,2, 1),\
-		.e		= bitcut(_access,3, 1),\
-		.s		= bitcut(_access,4, 1),\
-		.dpl		= bitcut(_access,5, 2),\
-		.p		= bitcut(_access,7, 1),\
-		.limit 		= bitcut(_limit, 16, 4),\
-		.__reserved 	= bitcut(_flags, 0, 1),\
-		.l		= bitcut(_flags, 1, 1),\
-		.db		= bitcut(_flags, 2, 1),\
-		.g		= bitcut(_flags, 3, 1),\
-		.base_high	= bitcut(_base, 24, 8)\
+#define gdt_make_entry(_base, _limit, _access, _flags)			\
+	{								\
+		.limit_low 	= bitcut(_limit, 0, 16),		\
+		.base_low 	= bitcut(_base, 0, 16),			\
+		.base_mid	= bitcut(_base, 16, 8),			\
+		.access 	= _access,				\
+		.flags		= (_flags << 4) | bitcut(_limit, 16, 4),\
+		.base_high	= bitcut(_base, 24, 8)			\
 	}
 
-struct gdt_node{
+struct gdt_entry{
 	u16 limit_low;
 	u16 base_low;
 	u8 base_mid;
-	u8 a		: 1,
-	   rw		: 1,
-	   dc		: 1,
-	   e		: 1,
-	   s		: 1,
-	   dpl		: 2,
-	   p		: 1;
-
-	u8 limit 	: 4,
-	   __reserved	: 1,
-	   l		: 1,
-	   db		: 1,
-	   g		: 1;
+	u8 access;
+	u8 flags;
 	u8 base_high;
 } __packed;
 
-__ASSERT(GDT_SZENT == sizeof(struct gdt_node));
+__ASSERT(GDT_SZENT == sizeof(struct gdt_entry));
 
 struct gdt_ptr{
 	u16 size;
 	u32 ptr;
 } __packed;
 
-extern struct gdt_ptr gdt_ptr;
-extern struct gdt_node gdt[GDT_NENT];
-
-/* seg_reload.S */
-void flush_gdt(struct gdt_ptr*);
+extern struct gdt_ptr 	gdt_ptr;
+extern struct gdt_entry gdt[GDT_NENT];
 
 #endif /*  __ASSEMBLY__ */
 #endif /* __H_GDT_H */
